74f0343ad17aa28a23d30162df7c1d992638adda
matitab <109882306+matitab@users.noreply.github.com>
2025-07-24 12:18:56 -0300

    Update particulas-config.js

--- particulas-config.js
+++ particulas-config.js
@@ -1,4 +1,4 @@
-tsParticles.load("tsparticles",{particles:{number:{value:40,density:{enable:true,value_area:800}},
+tsParticles.load("tsparticles",{particles:{number:{value:60,density:{enable:true,value_area:800}},
 			color:{value: ["#ffffff", "#ddeeff", "#cceeff"]},
 			shape:{type:"circle",stroke:{width:0}},opacity:{value:0.8,random:true, anim:{enable:true,speed:1,opacity_min:0.1,sync:false}},
 			size:{value:3.5,random:true,anim:{enable:false,speed:4,size_min:0.8,sync:false}},line_linked:{enable:false},
