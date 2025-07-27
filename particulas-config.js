18fe0810bcc005daf383763a3422d58ef41e4487
matitab <matitab013@gmail.com>
2025-07-26 21:05:49 -0300

    simplifico visualmente

--- particulas-config.js
+++ particulas-config.js
@@ -1,5 +1,5 @@
 tsParticles.load("tsparticles",{particles:{number:{value:150,density:{enable:true,value_area:800}},
-			color:{value: ["#ffffff", "#ddeeff", "#cceeff"]},
+			color:{value: ["#000000ff", "#313233ff", "#cceeff"]},
 			shape:{type:"circle",stroke:{width:0}},opacity:{value:0.8,random:true, anim:{enable:true,speed:1,opacity_min:0.1,sync:false}},
 			size:{value:3,random:true,anim:{enable:false,speed:4,size_min:0.4,sync:false}},line_linked:{enable:false},
 			shadow: {enable: true, color: "#ffffffff", blur: 10},
