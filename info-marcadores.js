6ce56ec637d817b394fc52e6ff849e079ba1c9f8
matitab <matitab013@gmail.com>
2025-07-28 04:39:56 -0300

    backtings en info-marcadores.js

---
+++ info-marcadores.js
@@ -0,0 +1,28 @@
+/* Información sobre cada marcador */
+export const marcadoresData = [
+{
+nombre: "Nombre y apellido 0",
+info: `Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
+Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
+Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
+Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
+Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.`,
+historia: "Historia 0",
+imagen: "assets/imagentest1.png",
+video: "assets/video1test.mp4"
+},
+{
+nombre: "Nombre y apellido 1",
+info: "Información 1",
+historia: "Historia 1",
+imagen: "assets/imagentest1.png",
+video: "assets/video1test.mp4"
+},
+{
+nombre: "Nombre y apellido 2",
+info: "Información 2",
+historia: "Historia 2",
+imagen: "assets/imagentest1.png",
+video: "assets/video1test.mp4"
+}
+];
