3d799b2ed3e620092ddb46d7c8d35b77696cad5e
matitab <matitab013@gmail.com>
2025-08-04 01:28:35 -0300

    elimina líneas de arranque.js

--- js/arranque.js
+++ js/arranque.js
@@ -6,11 +6,8 @@ const pantalla = document.getElementById("pantalla-carga");
 const background = document.getElementById("img-fondo");
 const escena = document.querySelector("a-scene");
 escena.addEventListener("renderstart", () => {
-pantalla.style.display = "flex";
-boton.style.display = "inline-block";
 loader.classList.add("fade-out-loader");
 loader.addEventListener("transitionend", () => {
-loader.style.display = "none";
 }, { once: true });
 pantalla.classList.remove("fade-out");});
 // Botón "Empezar recorrido" activa MindAR y oculta la pantalla de carga
@@ -18,8 +15,5 @@ boton.addEventListener('click', () => {
 pantalla.classList.add('fade-out');
 background.classList.add('fade-out');
 pantalla.addEventListener('transitionend', () => {
-pantalla.style.display = 'none';
-background.style.display = 'none';
 }, {once: true});
-escena.components['mindar-image'].start();});
-}
+escena.components['mindar-image'].start();});}
