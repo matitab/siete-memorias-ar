474d3f19905a4cdbb8b1cd67140a5bba72d0dfef
matitab <matitab013@gmail.com>
2025-08-04 18:14:38 -0300

    a ver si carga

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,4 +1,4 @@
 export function fullscreen(){if(document.fullscreenEnabled){const fullscreen_button=document.createElement("button");fullscreen_button.setAttribute('id','fullscreen-button');fullscreen_button.addEventListener("click",toggle_fullscreen);fullscreen_button.innerHTML=`
-        <img src="/assets/fullscreen.png" alt="Fullscreen">
+        <img src="ocho-memorias-ar/assets/fullscreen.png">
     `;document.body.appendChild(fullscreen_button)}
 function toggle_fullscreen(){if(!document.fullscreenElement){document.body.requestFullscreen();document.body.setAttribute("fullscreen","")}else{document.exitFullscreen();document.body.removeAttribute("fullscreen")}}}
