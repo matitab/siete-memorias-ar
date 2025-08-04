f797a8f35a0ee0030c4e03ed30ceed8df162aa92
matitab <matitab013@gmail.com>
2025-08-04 06:58:05 -0300

    botón comenzar recorrido aparece recién al terminar de cargar la escena AR

--- js/arranque.js
+++ js/arranque.js
@@ -6,14 +6,25 @@ const pantalla = document.getElementById("pantalla-carga");
 const background = document.getElementById("img-fondo");
 const escena = document.querySelector("a-scene");
 escena.addEventListener("renderstart", () => {
+pantalla.style.display = "flex";
+background.style.display = "block";
 loader.classList.add("fade-out-loader");
-loader.addEventListener("transitionend", () => {
+boton.classList.add("fade-in");
+boton.addEventListener("transitionend", () => {
+boton.style.pointerEvents = "auto";
 }, { once: true });
-pantalla.classList.remove("fade-out");});
-// Botón "Empezar recorrido" activa MindAR y oculta la pantalla de carga
+// Botón "Empezar recorrido" aparece recién al cargar la escena AR
+loader.addEventListener("transitionend", () => {
+loader.style.display = "none";
+}, { once: true });
+pantalla.classList.remove("fade-out");
+});
+// Al clickearlo, activa MindAR y oculta la pantalla de carga
 boton.addEventListener('click', () => {
 pantalla.classList.add('fade-out');
 background.classList.add('fade-out');
 pantalla.addEventListener('transitionend', () => {
-}, {once: true});
-escena.components['mindar-image'].start();});}
+pantalla.style.display = 'none';
+background.style.display = 'none';
+escena.components['mindar-image'].start();
+}, { once: true });});}
