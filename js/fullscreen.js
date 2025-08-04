1b44195846e82aa4308fc7ac6139178af6ad375c
matitab <matitab013@gmail.com>
2025-08-04 19:10:24 -0300

    posible fix fullscreen btn

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,4 +1,7 @@
 export function fullscreen(){if(document.fullscreenEnabled){const fullscreen_button=document.createElement("button");fullscreen_button.setAttribute('id','fullscreen-button');fullscreen_button.addEventListener("click",toggle_fullscreen);fullscreen_button.innerHTML=`
-        <img src="ocho-memorias-ar/assets/fullscreen.png">
+        <img width: 25px;
+height: 25px;
+background-color: transparent;
+pointer-events: none; src="assets/fullscreen.png">
     `;document.body.appendChild(fullscreen_button)}
 function toggle_fullscreen(){if(!document.fullscreenElement){document.body.requestFullscreen();document.body.setAttribute("fullscreen","")}else{document.exitFullscreen();document.body.removeAttribute("fullscreen")}}}
