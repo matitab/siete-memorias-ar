9b508fdda28489260ba786debbb8e459110be3a5
matitab <matitab013@gmail.com>
2025-08-07 21:11:35 -0300

    await import p/cargar contenido recién al presionar item nav

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 import {ContadorMarcadores} from './contador.js';
 import {DetectarMarcador} from './DetectarMarcador.js';
 import {arranque} from './arranque.js';
-import {mostrarContenido} from './mostrarContenido.js';
 import { fullscreen } from './fullscreen.js';
 fullscreen();
 arranque();
 ContadorMarcadores();
 document.querySelectorAll(".nav-item").forEach(boton => {
-boton.addEventListener("click", () => {
+boton.addEventListener("click", async () => {
 const opcion = boton.textContent.trim();
+const {mostrarContenido} = await import('./mostrarContenido.js');
 mostrarContenido(opcion, boton);
 });});
 DetectarMarcador();
