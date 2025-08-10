31ff65c8f689759b0a4155bf811305b994ba8ba1
matitab <matitab013@gmail.com>
2025-08-10 04:01:23 -0300

    quito import innecesario de main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /* Motor del programa */
 import {arranque} from './arranque.js';
 import { fullscreen } from './fullscreen.js';
-import { DetectarMarcador } from './DetectarMarcador.js';
 fullscreen();
 arranque();
