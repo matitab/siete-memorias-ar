a85a0d469e2838af75a1fcfffeb07af8d0b612a4
matitab <matitab013@gmail.com>
2025-08-10 05:14:09 -0300

    contador visible nuevamente

--- js/arranque.js
+++ js/arranque.js
@@ -5,16 +5,16 @@ const boton = document.getElementById("boton-comenzar");
 const pantalla = document.getElementById("pantalla-carga");
 const background = document.getElementById("img-fondo");
 const escena = document.querySelector("a-scene");
-escena.addEventListener("renderstart", () => {
-pantalla.style.display = "flex";
-background.style.display = "block";
+escena.addEventListener("renderstart", async() => {
 loader.classList.add("fade-out-loader");
-boton.classList.add("fade-in");
-boton.style.pointerEvents = "auto";
-// Botón "Empezar recorrido" aparece recién al cargar la escena AR
 loader.addEventListener("transitionend", () => {
 loader.style.display = "none";
 }, { once: true });
+const {fullscreen} = await import('./fullscreen.js');//aparece el btn de pantalla completa
+fullscreen();
+// Botón "Empezar recorrido" aparece recién al cargar la escena AR
+boton.classList.add("fade-in");
+boton.style.pointerEvents = "auto";
 pantalla.classList.remove("fade-out");
 });
 // Al clickearlo, activa MindAR y oculta la pantalla de carga
@@ -24,7 +24,9 @@ pantalla.classList.add('fade-out');
 background.classList.add('fade-out');
 pantalla.addEventListener('transitionend', async () => {
 const {DetectarMarcador} = await import('./DetectarMarcador.js');
+const {ContadorMarcadores} = await import('./contador.js');
 DetectarMarcador();
+ContadorMarcadores();
 escena.components['mindar-image'].start();
 }, { once: true });
 const {menuAbierto, menuCerrado} = await import('./openclose-menu.js');
--- js/contador.js
+++ js/contador.js
@@ -2,20 +2,20 @@
 export function ContadorMarcadores() {
 	const marcadorDetectado = new Set(); // índices de los target ya escaneados
 	const totalMarcadores = 3;
-	const contadorDiv = document.getElementById("contador-marcadores");
-    contadorDiv.addEventListener("click", ()=>{
-    contadorDiv.style.display="none";
+	const contador = document.getElementById("contador-marcadores");
+    contador.addEventListener("click", ()=>{ // cierra mensaje con click
+    contador.style.display="none";
   });
 	const actualizarContador = () => {
-  contadorDiv.textContent = `Escaneaste ${marcadorDetectado.size} de ${totalMarcadores} imágenes`;
-  contadorDiv.style.display = "block";
-  clearTimeout(contadorDiv.hideTimeout);
-  contadorDiv.hideTimeout = setTimeout(() => {
-    contadorDiv.style.display = "none";
+  contador.textContent = `Escaneaste ${marcadorDetectado.size} de ${totalMarcadores} imágenes`;
+  contador.style.display = "block";
+  clearTimeout(contador.hideTimeout);
+  contador.hideTimeout = setTimeout(() => {
+    contador.style.display = "none";
   }, 3000);
   if (marcadorDetectado.size === totalMarcadores) {
     setTimeout(() => {
-      document.getElementById("mensaje-final").style.display = "block";
+      document.getElementById("mensaje-final").style.display = "block"; //muestra mensaje final
     }, 500);}};
 // eventos de detección (si agregamos más targets, modificar índice)
 [0, 1, 2].forEach(index => {
