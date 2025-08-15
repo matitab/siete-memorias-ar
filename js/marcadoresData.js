1ce60b1e551cf4c353b407eb9060485c8b5ed0cb
matitab <matitab013@gmail.com>
2025-08-15 16:37:55 -0300

    marcadores actualizados!

--- js/marcadoresData.js
+++ js/marcadoresData.js
@@ -1,28 +1,24 @@
 /* Información sobre cada marcador */
 export const marcadoresData = [
 {
-nombre: "Nombre y apellido 0",
-info: `Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
-Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
-Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
-Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.
-Proyecto basado en el escaneo de imágenes para acceder a una experiencia de realidad aumentada.`,
-historia: "Historia 0",
-imagen: "assets/imagentest1.png",
-video: "assets/video1test.mp4"
+nombre: "Yanina García",
+info: `Proximamente.`,
+historia: "Proximamente.",
+imagen: "assets/1.jpg",
+video: "Proximamente."
 },
 {
 nombre: "Nombre y apellido 1",
-info: "Información 1",
-historia: "Historia 1",
-imagen: "assets/imagentest1.png",
-video: "assets/video1test.mp4"
+info: "Los días 19 y 20 fueron días de agitación civil, saqueos y represión por parte de la policía y las fuerzas federales.",
+historia: "Los días 19 y 20 fueron días de agitación civil, saqueos y represión por parte de la policía y las fuerzas federales.",
+imagen: "assets/2.jpg",
+video: "´Proximamente."
 },
 {
 nombre: "Nombre y apellido 2",
-info: "Información 2",
-historia: "Historia 2",
-imagen: "assets/imagentest1.png",
-video: "assets/video1test.mp4"
+info: "Todos los años, los familiares se encuentran frente a tribunales para exigir justicia.",
+historia: "Todos los años, los familiares se encuentran frente a tribunales para exigir justicia.",
+imagen: "assets/3.jpg",
+video: "Proximamente."
 }
 ];
