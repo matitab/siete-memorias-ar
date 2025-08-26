5a46f3c7eb436227dc882c6998997a90ecee0350
matitab <matitab013@gmail.com>
2025-08-26 02:13:06 -0300

    deshago cambios mostrarcontenido.js

--- js/marcadoresData.js
+++ js/marcadoresData.js
@@ -3,13 +3,22 @@ export const marcadoresData = [
 {
 nombre: "Yanina García",
 info: `Proximamente.`,
+historia: "Proximamente.",
+imagen: "assets/1.jpg",
+video: "Proximamente."
 },
 {
 nombre: "Nombre y apellido 1",
 info: "Los días 19 y 20 fueron días de agitación civil, saqueos y represión por parte de la policía y las fuerzas federales.",
+historia: "Los días 19 y 20 fueron días de agitación civil, saqueos y represión por parte de la policía y las fuerzas federales.",
+imagen: "assets/2.jpg",
+video: "´Proximamente."
 },
 {
 nombre: "Nombre y apellido 2",
 info: "Todos los años, los familiares se encuentran frente a tribunales para exigir justicia.",
+historia: "Todos los años, los familiares se encuentran frente a tribunales para exigir justicia.",
+imagen: "assets/3.jpg",
+video: "Proximamente."
 }
 ];
