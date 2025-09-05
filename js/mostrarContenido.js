67cd0b32ab5f43f22ca644a9bd38466e3c4d608a
matitab <109882306+matitab@users.noreply.github.com>
2025-09-05 16:24:11 -0300

    Update mostrarContenido.js

--- js/mostrarContenido.js
+++ js/mostrarContenido.js
@@ -5,8 +5,6 @@ export function mostrar_contenido(opc, element) {
 const marcadorActivo = getMarcadorActivo();
 const contenido = document.getElementById(`menu-content${marcadorActivo}`);
 const menuActivo = document.getElementById(`Menu${marcadorActivo}`);
-if (menuActivo) menuActivo.querySelectorAll(".nav-item").forEach(boton => boton.classList.remove("cliqueado"));
-if (element) element.classList.add("cliqueado");
 const data = marcadoresData[marcadorActivo];
 let contenido_mostrado = "";
 switch (opc) {
@@ -20,8 +18,6 @@ contenido_mostrado = `
   <video src="${data.video}" controls style="width: 100%; height: auto; border-radius: .2em; display: block;"></video>
   `;
 break;
-default:
-contenido_mostrado = "<p>Selecciona una opción del menú</p>";
 }
 contenido.innerHTML = contenido_mostrado;
 }
